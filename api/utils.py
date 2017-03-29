ef88efb871b231dd069146a36a85094459f32c85
Thiago Chagas <tqchagas@gmail.com>
2017-03-29 09:27:45 -0300

    salva as reservas de uma data

--- api/utils.py
+++ api/utils.py
@@ -19,3 +19,24 @@ def buscar_reservas(data, timeout=5):
         return []
     except requests.exceptions.ConnectionError:
         raise Exception("Erro de conexão")
+
+
+def salvar_reservas(data):
+    # salva os dados da reserva de uma data
+    reservas, carros_reservados = buscar_reservas(data), []
+    consulta = Consulta.objects.create(data_consultada=data)
+    for reserva in reservas:
+        obj_montadora, criado = Montadora.objects.get_or_create(
+            nome=reserva.get('brand')
+        )
+        obj_modelo, criado = Modelo.objects.get_or_create(
+            nome=reserva.get('model'),
+            montadora=obj_montadora
+        )
+        reserva = Reserva(
+            consulta=consulta,
+            modelo=obj_modelo,
+        )
+        carros_reservados.append(reserva)
+
+    return Reserva.objects.bulk_create(carros_reservados)
